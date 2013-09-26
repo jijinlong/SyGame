489103337666309a403d5ecfd55ff1f102231353
jijinlong <jjl_2009_hi@163.com>
2013-09-26 17:09:10 +0800

    BitMap  增加位串中设置 获取32位字符
    lua 中使用时可以分析报文的每个位

--- bitmap/bitmap/BitMap.h
+++ bitmap/bitmap/BitMap.h
@@ -22,6 +22,32 @@ public:
 		tags[blockid] |= (1 << offset);
 		if (index > maxSize) maxSize = index;
 	}
+	void set(unsigned int startindex,unsigned int size,unsigned int value)
+	{
+		// 一个低效的办法
+		for (unsigned int index = 0 ; index < sizeof(unsigned int ) * 8 && index < size;index++)
+		{
+			if ((value >> index) & 1)
+			{
+				set(startindex + index);
+			}
+			else
+				reset(startindex + index);
+		}
+	}
+	unsigned get(unsigned int startindex,unsigned int size)
+	{
+		// 一个低效的办法
+		int value = 0;
+		for (unsigned int index = startindex; index < startindex + size;index++)
+		{
+			if (test(index))
+			{
+				value |= 1 << (index - startindex);
+			}
+		}
+		return value;
+	}
 	void reset(unsigned int index)
 	{
 		int blockid = getBlockId(index);
@@ -53,7 +79,43 @@ public:
 	{
 		return maxSize;
 	}
-private:
+	/**
+	 * 可能弃用
+	 **/
+	void copyto(unsigned char*buffer,unsigned int buffersize)
+	{
+		// 低效做法 按字节拷贝
+		unsigned int bytes = 0;
+		for(std::vector<unsigned int>::iterator iter = tags.begin(); iter != tags.end() && bytes < buffersize;++iter)
+		{
+			unsigned int value = *iter;
+			for (int index = 0; index < sizeof(unsigned int) && bytes < buffersize;index++)
+			{
+				*buffer = (value) >> (8 * index);
+				buffer++;
+				bytes++;
+			}
+		}
+	}
+	/**
+	 * 可能弃用
+	 **/
+	void copyfrom(unsigned char*buffer,unsigned int buffersize)
+	{
+		tags.clear();
+		// 低效做法 按字节拷贝
+		int i = 0;
+		while (i ++ < buffersize)
+		{
+			unsigned int value = 0;
+			for (int index = 0; index < sizeof(unsigned int);index++)
+			{
+				value |= ((int)*buffer) << (8 * index);
+				buffer++;
+			}
+			tags.push_back(value);
+		}
+	}
 	std::vector<unsigned int> tags;
 	unsigned int maxSize;
 };
--- bitmap/bitmap/testBitMap.cpp
+++ bitmap/bitmap/testBitMap.cpp
@@ -14,4 +14,14 @@ int main()
 	{
 		printf("hellow2 %d\n",map.capacity());
 	}
+	map.set(0,32,111112127);
+	int value = map.get(0,32);
+	printf("value:%d",value);
+	BitMap next;
+	unsigned char buffer[200] = {'h','w','l','\0'};
+	next.copyfrom(buffer,4);
+	
+	unsigned char buffer1[200] ={'\0'};
+	next.copyto(buffer1,4);
+	printf("next:value:%s",buffer1);
 }
